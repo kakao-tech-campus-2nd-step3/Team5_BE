aa605c51d6808dda38380a53dd1ed67d60ce9fba
xkrwjdals <developjmt@gmail.com>
2024-09-18 23:48:30 +0900

    Feat: Reaction 엔티티 클래스 정의

---
+++ src/main/java/ojosama/talkak/reaction/Reaction.java
@@ -0,0 +1,61 @@
+package ojosama.talkak.reaction;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import ojosama.talkak.user.Member;
+import ojosama.talkak.video.Video;
+
+@Entity
+@Table(name = "reaction")
+public class Reaction {
+
+    @EmbeddedId
+    private ReactionId id;  // 복합 키를 포함하는 필드
+
+    private boolean reaction;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member", insertable = false, updatable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "video", insertable = false, updatable = false)
+    private Video video;
+
+    // Getters and setters
+    public ReactionId getId() {
+        return id;
+    }
+
+    public void setId(ReactionId id) {
+        this.id = id;
+    }
+
+    public boolean isReaction() {
+        return reaction;
+    }
+
+    public void setReaction(boolean reaction) {
+        this.reaction = reaction;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+}
