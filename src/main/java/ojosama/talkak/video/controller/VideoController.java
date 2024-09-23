8d854cc71fa5b109e45f864baf5a79d2b604ed56
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-09-24 03:15:57 +0900

    feat : Youtube Reference 관련 컨트롤러

--- src/main/java/ojosama/talkak/video/controller/VideoController.java
+++ src/main/java/ojosama/talkak/video/controller/VideoController.java
@@ -1,9 +1,12 @@
 package ojosama.talkak.video.controller;
 
+import java.io.IOException;
+import java.util.List;
+import ojosama.talkak.video.dto.YoutubeResponse;
 import ojosama.talkak.video.dto.YoutubeUrlValidationRequestDto;
 import ojosama.talkak.video.dto.YoutubeUrlValidationResponseDto;
-import ojosama.talkak.video.exception.YoutubeValidationException;
 import ojosama.talkak.video.service.VideoService;
+import ojosama.talkak.video.service.YoutubeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.*;
 public class VideoController {
 
     private final VideoService videoService;
+    private final YoutubeService youtubeService;
 
-    public VideoController(VideoService videoService) {
+    public VideoController(VideoService videoService, YoutubeService youtubeService) {
         this.videoService = videoService;
+        this.youtubeService = youtubeService;
     }
 
     @PostMapping("/youtube-valid")
@@ -22,4 +27,19 @@ public class VideoController {
         YoutubeUrlValidationResponseDto response = videoService.validateYoutubeUrl(req);
         return ResponseEntity.ok(response);
     }
+
+    // 메인페이지에서 유튜브 관련 영상 불러오기(카테고리 지정 X)
+    @GetMapping("/youtube")
+    public ResponseEntity<List<YoutubeResponse>> getPopularYoutubeShorts() throws IOException {
+        List<YoutubeResponse> response = youtubeService.getPopularShorts();
+        return ResponseEntity.ok(response);
+    }
+
+    // 메인페이지에서 유튜브 관련 영상 불러오기(카테고리 지정)
+    @GetMapping("/youtube/{category}")
+    public ResponseEntity<List<YoutubeResponse>> getPopularYoutubeShortsByCategory(@PathVariable("category") String category) throws IOException {
+        List<YoutubeResponse> response =  youtubeService.getShortsByCategory(category);
+        return ResponseEntity.ok(response);
+    }
+
 }
