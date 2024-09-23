a6df8178fb2ccb04661fb62bb870cfc7aab0b852
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-09-24 03:15:04 +0900

    feat : Youtube Reference 기능(인기 쇼츠 및 카테고리별 쇼츠 불러오기)

--- src/main/java/ojosama/talkak/video/service/YoutubeService.java
+++ src/main/java/ojosama/talkak/video/service/YoutubeService.java
@@ -1,2 +1,68 @@
-package ojosama.talkak.video.service;public class YoutubeService {
+package ojosama.talkak.video.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import ojosama.talkak.video.dto.YoutubeResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@PropertySource("classpath:application.properties")
+public class YoutubeService {
+    @Value("${youtube.api-key}")
+    private String apiKey;
+
+    private static final String YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/search";
+    private final RestTemplate restTemplate = new RestTemplate();
+    // 특정 카테고리가 없는 전체 유튜브 쇼츠 추출
+    @Cacheable("youtubeShorts")
+    public List<YoutubeResponse> getPopularShorts() throws IOException {
+        // 필요한 값들만 불러오도록 최소한의 필드 요청
+        String url = YOUTUBE_API_URL + "?part=snippet&fields=items(id(videoId),snippet(publishedAt, title, channelId, thumbnails(default(url))))&q=쇼츠&type=video&key="
+            +apiKey+"&maxResults=10";
+
+        String response = restTemplate.getForObject(url, String.class);
+        return parseYoutubeData(response);
+    }
+
+    // 특정 카테고리의 유튜브 쇼츠 추출
+    public List<YoutubeResponse> getShortsByCategory(String tag) throws IOException {
+        // 필요한 값들만 불러오도록 최소한의 필드 요청
+        String url = YOUTUBE_API_URL + "?part=snippet&fields=items(id(videoId),snippet(publishedAt, title, channelId, thumbnails(default(url))))&q=쇼츠, " + tag
+            + "&type=video&key=" + apiKey + "&maxResults=10";
+        String response = restTemplate.getForObject(url, String.class);
+        return parseYoutubeData(response);
+    }
+
+    // List 형식으로 변환
+    private List<YoutubeResponse> parseYoutubeData(String json) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(json);
+        List<YoutubeResponse> responses = new ArrayList<>();
+
+        for (JsonNode item : rootNode.path("items")) {
+            String videoId = item.path("id").path("videoId").asText();
+            String date = item.path("snippet").path("publishedAt").asText();
+            String channelId = item.path("snippet").path("channelId").asText();
+            String title = item.path("snippet").path("title").asText();
+            String thumnailUrl = item.path("snippet").path("thumbnails")
+                .path("default").path("url").asText();
+
+            DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+            LocalDateTime dateTime = LocalDateTime.parse(date, formatter);
+            responses.add(new YoutubeResponse(dateTime, videoId, channelId, title, thumnailUrl));
+        }
+
+        return responses;
+    }
+
+
 }
