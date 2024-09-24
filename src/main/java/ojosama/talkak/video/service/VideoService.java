69ac15a548ec89880f1e0288e1a3b1bd0d0a193c
Giheon Do <dgh0001@naver.com>
2024-09-24 10:23:16 +0900

    refactor: videoId 추출을 별도 클래스로 분리

--- src/main/java/ojosama/talkak/video/service/VideoService.java
+++ src/main/java/ojosama/talkak/video/service/VideoService.java
@@ -5,13 +5,12 @@ import ojosama.talkak.common.exception.code.VideoError;
 import ojosama.talkak.video.dto.YoutubeUrlValidationAPIResponseDto;
 import ojosama.talkak.video.dto.YoutubeUrlValidationRequestDto;
 import ojosama.talkak.video.dto.YoutubeUrlValidationResponseDto;
+import ojosama.talkak.video.util.IdExtractor;
 import ojosama.talkak.video.util.WebClientUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Service
 public class VideoService {
@@ -28,27 +27,24 @@ public class VideoService {
 
     public YoutubeUrlValidationResponseDto validateYoutubeUrl(YoutubeUrlValidationRequestDto req) {
         String url = req.url();
-        String videoId = Optional.ofNullable(extractVideoId(url)).orElseThrow(() -> new TalKakException(VideoError.VIDEO_NOT_FOUND));
+        String videoId = Optional.ofNullable(IdExtractor.extract(url))
+                .orElseThrow(() -> new TalKakException(VideoError.INVALID_VIDEO_ID));
 
 
-        StringBuilder YOUTUBE_API_URL = new StringBuilder();
-        YOUTUBE_API_URL.append(YOUTUBE_API_BASE_URL)
-                .append("&id=")
-                .append(videoId)
-                .append("&key=")
-                .append(YOUTUBE_API_KEY);
+        String YOUTUBE_API_URL = youtubeApiRequestUrlBuilder(videoId);
 
-        YoutubeUrlValidationAPIResponseDto response = webClientUtil.get(YOUTUBE_API_URL.toString(), YoutubeUrlValidationAPIResponseDto.class);
+        YoutubeUrlValidationAPIResponseDto response = webClientUtil.get(YOUTUBE_API_URL, YoutubeUrlValidationAPIResponseDto.class);
 
         return new YoutubeUrlValidationResponseDto(response);
     }
 
-    public String extractVideoId(String url) {
-        Pattern pattern = Pattern.compile("(?:v=|\\/)([a-zA-Z0-9_-]{11})", Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(url);
-        if (matcher.find()) {
-            return matcher.group(1);
-        }
-        return null;
+    private String youtubeApiRequestUrlBuilder(String videoId) {
+        StringBuilder youtubeApiRequestUrl = new StringBuilder();
+        youtubeApiRequestUrl.append(YOUTUBE_API_BASE_URL)
+                .append("&id=")
+                .append(videoId)
+                .append("&key=")
+                .append(YOUTUBE_API_KEY);
+        return youtubeApiRequestUrl.toString();
     }
 }
