ecd4c31060276908c91ff40b94be403d743ac332
Hyewon Seok <hws2008@naver.com>
2024-09-26 23:30:13 +0900

    Feat: 컨트롤러 진입 및 진출 로깅 추가

--- src/main/java/ojosama/talkak/common/aop/LoggingAspect.java
+++ src/main/java/ojosama/talkak/common/aop/LoggingAspect.java
@@ -1,16 +1,40 @@
 package ojosama.talkak.common.aop;
 
+import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import ojosama.talkak.common.exception.TalKakException;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
 @Component
 public class LoggingAspect {
+
+    @Pointcut("execution(* ojosama.talkak..controller..*(..))")
+    public void controllerPointCut() {
+    }
+
+    @Around("controllerPointCut()")
+    public Object logRequest(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object result = null;
+        long start = System.currentTimeMillis();
+        log.info("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+            joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        try {
+            result = joinPoint.proceed();
+        } finally {
+            long end = System.currentTimeMillis();
+            log.info("Exit : {}.{}() with result = {} ({}ms)", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), result, end - start);
+        }
+        return result;
+    }
 
     @AfterThrowing(pointcut = "execution(* ojosama.talkak..*(..))", throwing = "e")
     public void logException(JoinPoint joinPoint, TalKakException e) {
