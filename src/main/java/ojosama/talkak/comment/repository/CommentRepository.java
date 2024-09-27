672a14eb81ec7c0154c673a1d7a55e85fc4d5ae4
xkrwjdals <developjmt@gmail.com>
2024-09-27 14:09:01 +0900

    Refactor: CommentService 의 CRUD API 수정
    - createComment(), updateComment() 로 하나의 메서드가 하나의 기능을 담당하도록 분리
    - exception 발생 시 CommentError 사용
    - CommentRepository 에 memberId 와 videoId 로 Comment 를 찾는 메서드 추가

--- src/main/java/ojosama/talkak/comment/repository/CommentRepository.java
+++ src/main/java/ojosama/talkak/comment/repository/CommentRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByVideoId(long videoId);
+    Comment findByMemberIdAndVideoId(long memberId, long videoId);
 }
