2a1069ffb84a72349d574bff53f800eed2778900
Hyewon Seok <hws2008@naver.com>
2024-10-03 02:25:21 +0900

    Test: CORS 관련 테스트 추가

---
+++ src/test/java/ojosama/talkak/common/config/SecurityConfigTest.java
@@ -0,0 +1,55 @@
+package ojosama.talkak.common.config;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = SecurityConfig.class)
+@WebAppConfiguration
+class SecurityConfigTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                    .webAppContextSetup(context)
+                    .apply(SecurityMockMvcConfigurers.springSecurity())
+                    .build();
+    }
+
+    @Test
+    @DisplayName("CORS 설정 테스트")
+    void corsConfiguration() {
+        assertDoesNotThrow(() -> {
+            mockMvc.perform(options("/")
+                    .header("Origin", "https://somewhere.test")
+                    .header("Access-Control-Request-Method", "GET")
+                    .header("Access-Control-Request-Headers", "Test-Header")
+                )
+                .andExpect(status().isOk())
+                .andExpect(header().string("Access-Control-Allow-Origin", "https://somewhere.test"))
+                .andExpect(header().string("Access-Control-Allow-Methods", "GET"))
+                .andExpect(header().string("Access-Control-Allow-Headers", "Test-Header"))
+                .andExpect(header().string("Access-Control-Expose-Headers", "*"));
+        });
+    }
+}
