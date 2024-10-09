1a0444b32b9d2333d64942a27642bb3962d62f90
anacnoda77 <sm1108shin@naver.com>
2024-10-09 14:43:24 +0900

    Feature: 마이페이지 request 객체 validation 추가

--- src/main/java/ojosama/talkak/member/dto/MyPageInfoRequest.java
+++ src/main/java/ojosama/talkak/member/dto/MyPageInfoRequest.java
@@ -1,10 +1,16 @@
 package ojosama.talkak.member.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record MyPageInfoRequest(
+    @NotBlank
     String gender,
+    @NotBlank
     String age,
+    @NotEmpty @Size(min = 3, max = 3)
     List<Long> categories
 ) {
 
