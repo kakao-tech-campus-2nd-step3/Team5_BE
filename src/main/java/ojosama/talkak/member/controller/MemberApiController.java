c0df5df980bfbeaa988eeb78e0277493d946d2b9
Hyewon Seok <hws2008@naver.com>
2024-10-14 15:24:02 +0900

    Docs: 추가 정보 입력 swagger 응답 정보 추가

--- src/main/java/ojosama/talkak/member/controller/MemberApiController.java
+++ src/main/java/ojosama/talkak/member/controller/MemberApiController.java
@@ -1,9 +1,12 @@
 package ojosama.talkak.member.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import ojosama.talkak.common.exception.ErrorResponse;
 import ojosama.talkak.member.dto.AdditionalInfoRequest;
 import ojosama.talkak.member.dto.AdditionalInfoResponse;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +18,8 @@ public interface MemberApiController {
     @Operation(summary = "추가 정보 입력", description = "추가 정보를 입력합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "추가 정보 입력 성공"),
-            @ApiResponse(responseCode = "M001", description = "존재하지 않는 회원입니다."),
-            @ApiResponse(responseCode = "M002", description = "회원 정보를 수정하는 데 오류가 발생하였습니다.")
+            @ApiResponse(responseCode = "M001", description = "존재하지 않는 회원입니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "M002", description = "회원 정보를 수정하는 데 오류가 발생하였습니다.", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     ResponseEntity<AdditionalInfoResponse> updateAdditionalInfo(AdditionalInfoRequest request, Authentication authentication);
 }
