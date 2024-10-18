d49efd27bd9627de327783b43d901c8c941bb5b4
Hyewon Seok <hws2008@naver.com>
2024-10-18 16:36:27 +0900

    Fix: CORS 테스트 에러 수정

--- src/test/java/ojosama/talkak/auth/config/SecurityConfigTest.java
+++ src/test/java/ojosama/talkak/auth/config/SecurityConfigTest.java
@@ -8,19 +8,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = SecurityConfig.class)
-@WebAppConfiguration
+@SpringBootTest
+@ActiveProfiles("test")
 class SecurityConfigTest {
 
     @Autowired
